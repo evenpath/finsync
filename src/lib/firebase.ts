5f5426d56bd098b2bc4117a92a6ad0fbdb8e7e4c
Super Eats <supereats.in@gmail.com>
2025-08-04 15:29:12 +0000

    Login not working

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,5 @@
 // src/lib/firebase.ts
-import { initializeApp, getApps } from 'firebase/app';
+import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
@@ -13,8 +13,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const apps = getApps();
-const app = !apps.length ? initializeApp(firebaseConfig) : apps[0];
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 const auth = getAuth(app);
 const db = getFirestore(app);
