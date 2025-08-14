156051aaae90a1e9eeab960da087539fbd48b2a5
Super Eats <supereats.in@gmail.com>
2025-08-14 20:03:51 +0000

    it is not updated yet, not working it

--- src/app/(employee-auth)/login/page.tsx
+++ src/app/(employee-auth)/login/page.tsx
@@ -1,14 +1,13 @@
-
 // src/app/(employee-auth)/login/page.tsx
 "use client";
 
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { useToast } from "@/hooks/use-toast";
+import { Button } from '../../../components/ui/button';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '../../../components/ui/card';
+import { Input } from '../../../components/ui/input';
+import { Label } from '../../../components/ui/label';
+import { useToast } from '../../../hooks/use-toast';
 import { getAuth, RecaptchaVerifier, signInWithPhoneNumber, ConfirmationResult } from 'firebase/auth';
 import { app } from '@/lib/firebase';
 import { Phone, KeyRound, Building2, Users, ArrowRight } from 'lucide-react';
