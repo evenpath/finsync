ae33a7afd904c7bd1cd7b2ac74eeba2878b8d7f5
Super Eats <supereats.in@gmail.com>
2025-08-14 23:02:45 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -1,6 +1,5 @@
 "use client"
 
-
 import {
   Toast,
   ToastClose,
@@ -8,7 +7,8 @@ import {
   ToastProvider,
   ToastTitle,
   ToastViewport,
-} from "@/components/ui/toast"
+} from "./toast"
+import { useToast } from "../../hooks/use-toast"
 
 export function Toaster() {
   const { toasts } = useToast()
